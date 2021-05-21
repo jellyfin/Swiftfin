1c9b24a319b0bfcd9e9f9db75804f1c2732cdf5e
Aiden Vigue <acvigue@me.com>
2021-05-20 21:36:17 -0400

    Initial episode page

---
+++ JellyfinPlayer/EpisodeItemView.swift
@@ -0,0 +1,20 @@
+//
+//  EpisodeItemView.swift
+//  JellyfinPlayer
+//
+//  Created by Aiden Vigue on 5/20/21.
+//
+
+import SwiftUI
+
+struct EpisodeItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EpisodeItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        EpisodeItemView()
+    }
+}
