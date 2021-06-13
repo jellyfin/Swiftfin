114c9c661c1b70b75932b537c565fccf84b46550
acvigue <acvigue@users.noreply.github.com>
2021-06-13 02:17:32 +0000

    [create-pull-request] automated change

--- JellyfinPlayer/ImageView.swift
+++ JellyfinPlayer/ImageView.swift
@@ -13,16 +13,16 @@ import NukeUI
 struct ImageView: View {
     private var source: URL = URL(string: "https://example.com")!
     private var blurhash: String = "001fC^"
-    
+
     init(src: URL) {
         self.source = src
     }
-    
+
     init(src: URL, bh: String) {
         self.source = src
         self.blurhash = bh
     }
-    
+
     var body: some View {
         LazyImage(source: source, content: { state in
             if let image = state.image {
