5f61fcbec2eab6007864997640b7af8c8c9c2fdd
acvigue <acvigue@users.noreply.github.com>
2021-06-14 05:59:29 +0000

    [create-pull-request] automated change

--- JellyfinPlayer/VideoPlayer.swift
+++ JellyfinPlayer/VideoPlayer.swift
@@ -298,7 +298,7 @@ class PlayerViewController: UIViewController, VLCMediaDelegate, VLCMediaPlayerDe
                 }, receiveValue: { [self] response in
                     playSessionId = response.playSessionId ?? ""
 
-                    if(response.mediaSources == nil) {
+                    if response.mediaSources == nil {
                         delegate?.exitPlayer(self)
                         return
                     }
