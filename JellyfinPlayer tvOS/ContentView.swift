de632aeb7ef13db94ebd45aca384ab28e19f976b
Aiden Vigue <acvigue@me.com>
2021-06-15 19:56:49 -0400

    add some views - rename a few folders.

--- JellyfinPlayer tvOS/ContentView.swift
+++ JellyfinPlayer tvOS/ContentView.swift
@@ -11,11 +11,6 @@ import CoreData
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     var body: some View {
         NavigationView {
             List {
--- JellyfinPlayer tvOS/PersistenceController.swift
+++ JellyfinPlayer tvOS/PersistenceController.swift
@@ -31,7 +31,7 @@ struct PersistenceController {
     let container: NSPersistentContainer
 
     init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "JellyfinPlayer_tvOS")
+        container = NSPersistentContainer(name: "JellyfinPlayer")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
