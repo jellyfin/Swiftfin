fdfc7332a158378233083b15be45ec035c5552ad
Stephen Byatt <47413006+stephenb10@users.noreply.github.com>
2021-07-01 13:27:33 +1000

    Add up next view

---
+++ JellyfinPlayer tvOS/VideoPlayer/UpNextView.swift
@@ -0,0 +1,22 @@
+//
+ /* 
+  * SwiftFin is subject to the terms of the Mozilla Public
+  * License, v2.0. If a copy of the MPL was not distributed with this
+  * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+  *
+  * Copyright 2021 Aiden Vigue & Jellyfin Contributors
+  */
+
+import SwiftUI
+
+struct UpNextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpNextView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpNextView()
+    }
+}
