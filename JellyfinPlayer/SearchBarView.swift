356b9176f73fbcf46eb93ae3fa6d8198f85212be
PangMo5 <a3457709@icloud.com>
2021-07-15 23:51:58 +0900

    Redesign SearchBarView

--- JellyfinPlayer/SearchBarView.swift
+++ JellyfinPlayer/SearchBarView.swift
@@ -11,32 +11,25 @@ import SwiftUI
 
 struct SearchBar: View {
     @Binding var text: String
-
+    
     @State private var isEditing = false
-
+    
     var body: some View {
-        HStack {
-
+        HStack(spacing: 8) {
             TextField("Search...", text: $text)
                 .padding(7)
                 .padding(.horizontal, 16)
                 .background(Color(.systemGray6))
                 .cornerRadius(8)
-                .padding(.horizontal, 10)
-                .onTapGesture {
-                    self.isEditing = true
-                }
-
-            if isEditing {
+            if !text.isEmpty {
                 Button(action: {
-                    self.isEditing = false
                     self.text = ""
-
                 }) {
-                    Text("Cancel")
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.secondary)
                 }
-                .padding(.trailing, 10)
             }
         }
+        .padding(.horizontal, 16)
     }
 }
