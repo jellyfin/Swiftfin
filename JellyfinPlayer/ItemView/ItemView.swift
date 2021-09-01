4b0bcb9190f5abc640a669593ceefbdf472468fc
Ethan Pippin <ethanpippin2343@gmail.com>
2021-08-31 23:49:31 -0600

    Cleanup

--- JellyfinPlayer/ItemView/ItemView.swift
+++ JellyfinPlayer/ItemView/ItemView.swift
@@ -25,7 +25,7 @@ struct ItemNavigationView: View {
     
     var body: some View {
         ItemView(item: item)
-            .navigationBarTitle("", displayMode: .large)
+            .navigationBarTitle("", displayMode: .inline)
     }
 }
 
@@ -65,21 +65,5 @@ fileprivate struct ItemView: View {
                 .environmentObject(videoPlayerItem)
                 .environmentObject(viewModel)
         }
-    }
-}
-
-extension UINavigationBar {
-    static func changeAppearance(clear: Bool) {
-        let appearance = UINavigationBarAppearance()
-        
-        if clear {
-            appearance.configureWithTransparentBackground()
-        } else {
-            appearance.configureWithDefaultBackground()
-        }
-        
-        UINavigationBar.appearance().standardAppearance = appearance
-        UINavigationBar.appearance().compactAppearance = appearance
-        UINavigationBar.appearance().scrollEdgeAppearance = appearance
     }
 }
