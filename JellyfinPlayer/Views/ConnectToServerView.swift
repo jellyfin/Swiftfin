d84ff63edf210db0d41d61e0375279286dfef561
Ethan Pippin <ethanpippin2343@gmail.com>
2021-10-14 17:09:57 -0600

    Fix header

--- JellyfinPlayer/Views/ConnectToServerView.swift
+++ JellyfinPlayer/Views/ConnectToServerView.swift
@@ -1,4 +1,5 @@
-/* JellyfinPlayer/Swiftfin is subject to the terms of the Mozilla Public
+/*
+ * JellyfinPlayer/Swiftfin is subject to the terms of the Mozilla Public
  * License, v2.0. If a copy of the MPL was not distributed with this
  * file, you can obtain one at https://mozilla.org/MPL/2.0/.
  *
