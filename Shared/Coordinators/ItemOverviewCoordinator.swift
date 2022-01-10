c6886e3272e06b0e54db2e5cb643662d318031cd
Ethan Pippin <ethanpippin2343@gmail.com>
2022-01-09 20:00:41 -0700

    iOS truncated item overview

---
+++ Shared/Coordinators/ItemOverviewCoordinator.swift
@@ -0,0 +1,29 @@
+//
+ /* 
+  * SwiftFin is subject to the terms of the Mozilla Public
+  * License, v2.0. If a copy of the MPL was not distributed with this
+  * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+  *
+  * Copyright 2021 Aiden Vigue & Jellyfin Contributors
+  */
+
+import Stinsen
+import SwiftUI
+import JellyfinAPI
+
+final class ItemOverviewCoordinator: NavigationCoordinatable {
+
+    let stack = NavigationStack(initial: \ItemOverviewCoordinator.start)
+    
+    @Root var start = makeStart
+    
+    let item: BaseItemDto
+    
+    init(item: BaseItemDto) {
+        self.item = item
+    }
+    
+    @ViewBuilder func makeStart() -> some View {
+        ItemOverviewView(item: item)
+    }
+}
---
+++ Swiftfin/Views/ItemOverviewView.swift
@@ -0,0 +1,35 @@
+//
+ /* 
+  * SwiftFin is subject to the terms of the Mozilla Public
+  * License, v2.0. If a copy of the MPL was not distributed with this
+  * file, you can obtain one at https://mozilla.org/MPL/2.0/.
+  *
+  * Copyright 2021 Aiden Vigue & Jellyfin Contributors
+  */
+
+import JellyfinAPI
+import SwiftUI
+
+struct ItemOverviewView: View {
+    
+    @EnvironmentObject var itemOverviewRouter: ItemOverviewCoordinator.Router
+    let item: BaseItemDto
+    
+    var body: some View {
+        ScrollView(showsIndicators: false) {
+            Text(item.overview ?? "")
+                .font(.footnote)
+                .padding()
+        }
+        .navigationBarTitle("Overview", displayMode: .inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarLeading) {
+                Button {
+                    itemOverviewRouter.dismissCoordinator()
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                }
+            }
+        }
+    }
+}
