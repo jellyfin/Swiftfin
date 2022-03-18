a69fdb996411f692070d0babe64c21def1714902
PangMo5 <a3457709@icloud.com>
2022-03-19 03:22:55 +0900

    update contributing.md
    run swiftformat .

--- Shared/Extensions/Defaults+Workaround.swift
+++ Shared/Extensions/Defaults+Workaround.swift
@@ -10,37 +10,37 @@ import Defaults
 import Foundation
 
 public extension Defaults.Serializable where Self: Codable {
-    static var bridge: Defaults.TopLevelCodableBridge<Self> { Defaults.TopLevelCodableBridge() }
+	static var bridge: Defaults.TopLevelCodableBridge<Self> { Defaults.TopLevelCodableBridge() }
 }
 
 public extension Defaults.Serializable where Self: Codable & NSSecureCoding {
-    static var bridge: Defaults.CodableNSSecureCodingBridge<Self> { Defaults.CodableNSSecureCodingBridge() }
+	static var bridge: Defaults.CodableNSSecureCodingBridge<Self> { Defaults.CodableNSSecureCodingBridge() }
 }
 
 public extension Defaults.Serializable where Self: Codable & NSSecureCoding & Defaults.PreferNSSecureCoding {
-    static var bridge: Defaults.NSSecureCodingBridge<Self> { Defaults.NSSecureCodingBridge() }
+	static var bridge: Defaults.NSSecureCodingBridge<Self> { Defaults.NSSecureCodingBridge() }
 }
 
 public extension Defaults.Serializable where Self: Codable & RawRepresentable {
-    static var bridge: Defaults.RawRepresentableCodableBridge<Self> { Defaults.RawRepresentableCodableBridge() }
+	static var bridge: Defaults.RawRepresentableCodableBridge<Self> { Defaults.RawRepresentableCodableBridge() }
 }
 
 public extension Defaults.Serializable where Self: Codable & RawRepresentable & Defaults.PreferRawRepresentable {
-    static var bridge: Defaults.RawRepresentableBridge<Self> { Defaults.RawRepresentableBridge() }
+	static var bridge: Defaults.RawRepresentableBridge<Self> { Defaults.RawRepresentableBridge() }
 }
 
 public extension Defaults.Serializable where Self: RawRepresentable {
-    static var bridge: Defaults.RawRepresentableBridge<Self> { Defaults.RawRepresentableBridge() }
+	static var bridge: Defaults.RawRepresentableBridge<Self> { Defaults.RawRepresentableBridge() }
 }
 
 public extension Defaults.Serializable where Self: NSSecureCoding {
-    static var bridge: Defaults.NSSecureCodingBridge<Self> { Defaults.NSSecureCodingBridge() }
+	static var bridge: Defaults.NSSecureCodingBridge<Self> { Defaults.NSSecureCodingBridge() }
 }
 
 public extension Defaults.CollectionSerializable where Element: Defaults.Serializable {
-    static var bridge: Defaults.CollectionBridge<Self> { Defaults.CollectionBridge() }
+	static var bridge: Defaults.CollectionBridge<Self> { Defaults.CollectionBridge() }
 }
 
 public extension Defaults.SetAlgebraSerializable where Element: Defaults.Serializable & Hashable {
-    static var bridge: Defaults.SetAlgebraBridge<Self> { Defaults.SetAlgebraBridge() }
+	static var bridge: Defaults.SetAlgebraBridge<Self> { Defaults.SetAlgebraBridge() }
 }
