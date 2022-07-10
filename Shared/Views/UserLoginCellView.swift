fcc022ea226d3b9f92b6dcd6dbc017bed5f1759b
David Ullmer <davidullmer@outlook.de>
2022-07-10 18:47:52 +0200

    Format code

--- Shared/Views/UserLoginCellView.swift
+++ Shared/Views/UserLoginCellView.swift
@@ -6,65 +6,64 @@
 // Copyright (c) 2022 Jellyfin & Jellyfin Contributors
 //
 
-import SwiftUI
 import JellyfinAPI
+import SwiftUI
 
 struct UserLoginCellView: View {
-    
-    @State
-    private var expanded = false;
-    
-    @State
-    private var enteredPassword: String = ""
-    
-    var user: UserDto
-    var baseURL: String?
-    var loginTapped : (String, String) -> Void
-    var cancelTapped: () -> Void
-    
-    var body: some View {
-        DisclosureGroup() {
-            VStack(alignment: .leading, spacing: 16) {
-                SecureField(L10n.password, text: $enteredPassword)
-                Button {
-                    loginTapped(user.name ?? "", enteredPassword)
-                } label: {
-                    L10n.signIn.text
-                }
-            }
-            .padding(.leading, -16)
-        } label: {
-            HStack(spacing: 4.0) {
-                AsyncImage(
-                    url: getProfileImageUrl(),
-                    content: { image in
-                        image.resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 50, maxHeight: 50)
-                            .clipShape(Circle())
-                    },
-                    placeholder: {
-                        Image(systemName: "person.circle")
-                            .resizable()
-                            .font(.system(size: 40))
-                            .scaledToFit()
-                            .frame(maxWidth: 50, maxHeight: 50)
-                    })
-                .padding(.vertical, 4.0)
 
-                Text(user.name ?? "")
-                    .padding(.leading, 4.0)
-                Spacer()
-            }
-        }
-    }
-    
-    func getProfileImageUrl() -> URL? {
-        if let userId = user.id, let imageTag = user.primaryImageTag, let server = baseURL {
-            let url =  URL(string: "\(server)/Users/\(userId)/Images/Primary?width=200&tag=\(imageTag)&quality=90")
-            LogManager.log.debug(url?.absoluteString ?? "")
-            return url
-        }
-        return nil
-    }
+	@State
+	private var expanded = false
+
+	@State
+	private var enteredPassword: String = ""
+
+	var user: UserDto
+	var baseURL: String?
+	var loginTapped: (String, String) -> Void
+	var cancelTapped: () -> Void
+
+	var body: some View {
+		DisclosureGroup {
+			VStack(alignment: .leading, spacing: 16) {
+				SecureField(L10n.password, text: $enteredPassword)
+				Button {
+					loginTapped(user.name ?? "", enteredPassword)
+				} label: {
+					L10n.signIn.text
+				}
+			}
+			.padding(.leading, -16)
+		} label: {
+			HStack(spacing: 4.0) {
+				AsyncImage(url: getProfileImageUrl(),
+				           content: { image in
+				           	image.resizable()
+				           		.aspectRatio(contentMode: .fit)
+				           		.frame(maxWidth: 50, maxHeight: 50)
+				           		.clipShape(Circle())
+				           },
+				           placeholder: {
+				           	Image(systemName: "person.circle")
+				           		.resizable()
+				           		.font(.system(size: 40))
+				           		.scaledToFit()
+				           		.frame(maxWidth: 50, maxHeight: 50)
+				           })
+				           .padding(.vertical, 4.0)
+
+				Text(user.name ?? "")
+					.padding(.leading, 4.0)
+				Spacer()
+			}
+		}
+	}
+
+	func getProfileImageUrl() -> URL? {
+		if let userId = user.id, let imageTag = user.primaryImageTag, let server = baseURL {
+			let url = URL(string: "\(server)/Users/\(userId)/Images/Primary?width=200&tag=\(imageTag)&quality=90")
+			LogManager.log.debug(url?.absoluteString ?? "")
+			return url
+		}
+		return nil
+	}
 }
