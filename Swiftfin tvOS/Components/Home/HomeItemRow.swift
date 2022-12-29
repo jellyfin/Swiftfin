293ad596ee9659e223fdae51e0449ad4d208410a
Rasmus Krämer <git@rfk.io>
2022-12-29 19:36:33 +0100

    The title is now visible

--- Swiftfin tvOS/Components/Home/HomeItemRow.swift
+++ Swiftfin tvOS/Components/Home/HomeItemRow.swift
@@ -21,35 +21,46 @@ struct HomeItemRow: View {
     var body: some View {
         ZStack(alignment: .top) {
             ScrollView(.horizontal) {
-                LazyHStack(spacing: 40) {
+                LazyHStack(alignment: focusPrefix == "hero" ? .bottom : .center, spacing: 40) {
                     ForEach(items, id: \.id) { item in
                         let focusName = "\(focusPrefix)::\(item.id!)"
                         
-                        ImageView(item.landscapePosterImageSources(maxWidth: size.rawValue))
-                            .aspectRatio(16 / 9, contentMode: .fit)
-                            .cornerRadius(7.5)
-                            .frame(width: size.rawValue, height: (size.rawValue / (16 / 9)))
-                        
-                            .focusable()
-                            .focused(focusedImage, equals: focusName)
-                            .scaleEffect(focusedImage.wrappedValue == focusName ? 1.11 : 1, anchor: size == .five ? .bottom : .center)
-                            .animation(.easeInOut(duration: 0.25), value: focusedImage.wrappedValue == focusName)
-                            .overlay {
-                                if let progress = item.userData?.playedPercentage, progress != 0 {
-                                    VStack {
-                                        Spacer()
-                                        ProgressBar(progress: progress / 100)
-                                            .frame(height: 5)
+                        VStack {
+                            ImageView(item.landscapePosterImageSources(maxWidth: size.rawValue))
+                                .aspectRatio(16 / 9, contentMode: .fit)
+                                .cornerRadius(7.5)
+                                .frame(width: size.rawValue, height: (size.rawValue / (16 / 9)))
+                            
+                                .focusable()
+                                .focused(focusedImage, equals: focusName)
+                                .scaleEffect(focusedImage.wrappedValue == focusName ? 1.11 : 1, anchor: focusPrefix == "hero" ? .bottom : .center)
+                                .animation(.easeInOut(duration: 0.25), value: focusedImage.wrappedValue)
+                                .overlay {
+                                    if let progress = item.userData?.playedPercentage, progress != 0 {
+                                        VStack {
+                                            Spacer()
+                                            ProgressBar(progress: progress / 100)
+                                                .frame(height: 5)
+                                        }
+                                        .padding(10)
                                     }
-                                    .padding(10)
                                 }
-                            }
-                            .onTapGesture {
-                                router.route(to: \.item, item)
-                            }
+                                .onTapGesture {
+                                    router.route(to: \.item, item)
+                                }
+                            
+                            Text(item.displayName)
+                                .fixedSize()
+                                .font(.system(.caption, design: .rounded))
+                                .padding(.horizontal)
+                                .offset(y: focusPrefix != "hero" && focusedImage.wrappedValue == focusName ? 10 : 0)
+                                .animation(.easeInOut(duration: 0.25), value: focusedImage.wrappedValue)
+                                .foregroundColor(focusedImage.wrappedValue == focusName ? Color.primary : Color.gray)
+                        }
+                        .frame(width: size.rawValue)
                     }
                 }
-                .frame(height: (size.rawValue / (16 / 9)) * (size == .four ? 1.11 : 1.22))
+                .frame(height: (size.rawValue / (16 / 9)) * 1.22 + 22)
                 .padding(.leading, edgeInsets.leading)
                 .padding(.trailing, edgeInsets.trailing)
             }
