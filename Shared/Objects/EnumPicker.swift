3d99600298760e89634de8a6b8d2afe5eec8132f
Ethan Pippin <ethanpippin2343@gmail.com>
2023-05-03 23:23:34 -0600

    Update EnumPicker.swift

--- Shared/Objects/EnumPicker.swift
+++ Shared/Objects/EnumPicker.swift
@@ -9,21 +9,21 @@
 import SwiftUI
 
 struct EnumPicker<EnumType: CaseIterable & Displayable & Hashable>: View {
-    
+
     enum NoneStyle: Displayable {
-        
+
         case text
         case dash(Int)
         case custom(String)
-        
+
         var displayTitle: String {
             switch self {
             case .text:
                 return L10n.none
-            case .dash(let length):
+            case let .dash(length):
                 precondition(length >= 1, "Dash must have length of at least 1.")
                 return String(repeating: "-", count: length)
-            case .custom(let text):
+            case let .custom(text):
                 precondition(!text.isEmpty, "Custom text must have length of at least 1.")
                 return text
             }
@@ -39,12 +39,12 @@ struct EnumPicker<EnumType: CaseIterable & Displayable & Hashable>: View {
 
     var body: some View {
         Picker(title, selection: $selection) {
-            
+
             if hasNil {
                 Text(noneStyle.displayTitle)
                     .tag(nil as EnumType?)
             }
-            
+
             ForEach(EnumType.allCases.asArray, id: \.hashValue) {
                 Text($0.displayTitle)
                     .tag($0 as EnumType?)
@@ -54,30 +54,30 @@ struct EnumPicker<EnumType: CaseIterable & Displayable & Hashable>: View {
 }
 
 extension EnumPicker {
-    
+
     init(title: String, selection: Binding<EnumType?>) {
         self.title = title
         self._selection = selection
         self.hasNil = true
         self.noneStyle = .text
     }
-    
+
     init(title: String, selection: Binding<EnumType>) {
         self.title = title
-        
+
         let binding = Binding<EnumType?> {
             selection.wrappedValue
         } set: { newValue, _ in
             assert(newValue != nil, "Should not have nil new value with non-optional binding")
             selection.wrappedValue = newValue!
         }
-        
+
         self._selection = binding
 
         self.hasNil = false
         self.noneStyle = .text
     }
-    
+
     func noneStyle(_ newStyle: NoneStyle) -> Self {
         copy(modifying: \.noneStyle, with: newStyle)
     }
