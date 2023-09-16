f898b77f9506aeaa3da1adb40217ab6fdaff7cdc
Joe <jpkribs@outlook.com>
2023-09-15 21:11:50 -0600

    Enable/Disable Filters
    From the Customization > Filters option, enable or disable a filter.

--- Swiftfin/Components/FilterDrawerHStack/FilterDrawerHStack.swift
+++ Swiftfin/Components/FilterDrawerHStack/FilterDrawerHStack.swift
@@ -7,68 +7,91 @@
 //
 
 import JellyfinAPI
+import Defaults
 import SwiftUI
 
 struct FilterDrawerHStack: View {
 
+    @Default(.Customization.Filters.showGenres)
+    private var filterShowGenres
+
+    @Default(.Customization.Filters.showFilters)
+    private var filterShowFilters
+
+    @Default(.Customization.Filters.showOrder)
+    private var filterShowOrder
+
+    @Default(.Customization.Filters.showSort)
+    private var filterShowSort
+    
     @ObservedObject
     private var viewModel: FilterViewModel
 
     private var onSelect: (FilterCoordinator.Parameters) -> Void
 
     var body: some View {
-        HStack {
-            if viewModel.currentFilters.hasFilters {
-                Menu {
-                    Button(role: .destructive) {
-                        viewModel.currentFilters = .init()
+        if filterShowOrder || filterShowOrder || filterShowOrder || filterShowOrder {
+            HStack {
+                if viewModel.currentFilters.hasFilters {
+                    Menu {
+                        Button(role: .destructive) {
+                            viewModel.currentFilters = .init()
+                        } label: {
+                            L10n.reset.text
+                        }
                     } label: {
-                        L10n.reset.text
+                        FilterDrawerButton(systemName: "line.3.horizontal.decrease.circle.fill", activated: true)
                     }
-                } label: {
-                    FilterDrawerButton(systemName: "line.3.horizontal.decrease.circle.fill", activated: true)
+                }
+    
+                if filterShowGenres {
+                    FilterDrawerButton(title: L10n.genres, activated: viewModel.currentFilters.genres != [])
+                        .onSelect {
+                            onSelect(.init(
+                                title: L10n.genres,
+                                viewModel: viewModel,
+                                filter: \.genres,
+                                selectorType: .multi
+                            ))
+                        }
+                }
+    
+                if filterShowFilters {
+                    FilterDrawerButton(title: L10n.filters, activated: viewModel.currentFilters.filters != [])
+                        .onSelect {
+                            onSelect(.init(
+                                title: L10n.filters,
+                                viewModel: viewModel,
+                                filter: \.filters,
+                                selectorType: .multi
+                            ))
+                        }
+                }
+    
+                if filterShowOrder {
+                    FilterDrawerButton(title: L10n.order, activated: viewModel.currentFilters.sortOrder != [APISortOrder.ascending.filter])
+                        .onSelect {
+                            onSelect(.init(
+                                title: L10n.order,
+                                viewModel: viewModel,
+                                filter: \.sortOrder,
+                                selectorType: .single
+                            ))
+                        }
+                }
+    
+                if filterShowOrder {
+                    FilterDrawerButton(title: L10n.sort, activated: viewModel.currentFilters.sortBy != [SortBy.name.filter])
+                        .onSelect {
+                            onSelect(.init(
+                                title: L10n.sort,
+                                viewModel: viewModel,
+                                filter: \.sortBy,
+                                selectorType: .single
+                            ))
+                        }
                 }
             }
-
-            FilterDrawerButton(title: L10n.genres, activated: viewModel.currentFilters.genres != [])
-                .onSelect {
-                    onSelect(.init(
-                        title: L10n.genres,
-                        viewModel: viewModel,
-                        filter: \.genres,
-                        selectorType: .multi
-                    ))
-                }
-
-            FilterDrawerButton(title: L10n.filters, activated: viewModel.currentFilters.filters != [])
-                .onSelect {
-                    onSelect(.init(
-                        title: L10n.filters,
-                        viewModel: viewModel,
-                        filter: \.filters,
-                        selectorType: .multi
-                    ))
-                }
-
-            FilterDrawerButton(title: L10n.order, activated: viewModel.currentFilters.sortOrder != [APISortOrder.ascending.filter])
-                .onSelect {
-                    onSelect(.init(
-                        title: L10n.order,
-                        viewModel: viewModel,
-                        filter: \.sortOrder,
-                        selectorType: .single
-                    ))
-                }
-
-            FilterDrawerButton(title: L10n.sort, activated: viewModel.currentFilters.sortBy != [SortBy.name.filter])
-                .onSelect {
-                    onSelect(.init(
-                        title: L10n.sort,
-                        viewModel: viewModel,
-                        filter: \.sortBy,
-                        selectorType: .single
-                    ))
-                }
         }
     }
 }
