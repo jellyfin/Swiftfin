36bba14a408adc71632cec0cdfe575c13450c6ec
Ethan Pippin <ethanpippin2343@gmail.com>
2023-10-30 15:28:49 -0600

    Update PagingLibraryView.swift

--- Swiftfin/Components/PagingLibraryView.swift
+++ Swiftfin/Components/PagingLibraryView.swift
@@ -63,7 +63,7 @@ struct PagingLibraryView: View {
     @ViewBuilder
     private var libraryGridView: some View {
         CollectionView(items: viewModel.items.elements) { _, item, _ in
-            PosterButton(state: .item(item), type: libraryGridPosterType)
+            PosterButton(item: item, type: libraryGridPosterType)
                 .scaleItem(libraryGridPosterType == .landscape && UIDevice.isPhone ? 0.85 : portraitPosterScale)
                 .onSelect {
                     onSelect(item)
