fb3f9c755a89a39da5d0cc9633c66d9a061f8665
alasclar <alastairwrawes@outlook.com>
2024-02-15 16:20:48 +0000

    Remove unneeded extra checks

--- Swiftfin/Views/ConnectToServerView.swift
+++ Swiftfin/Views/ConnectToServerView.swift
@@ -85,7 +85,7 @@ struct ConnectToServerView: View {
                 } label: {
                     L10n.connect.text
                 }
-                .disabled((URL(string: url) == nil && URL(string: "http://" + url) == nil) || isConnecting || url == "")
+                .disabled(URL(string: url) == nil || isConnecting)
             }
         } header: {
             L10n.connectToJellyfinServer.text
