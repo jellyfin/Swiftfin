733a7bf2a99409cfe2a43f5ac122ae61b5f1ec8b
Ethan Pippin <ethanpippin2343@gmail.com>
2024-03-16 22:11:04 -0600

    wip

--- Shared/Extensions/ViewExtensions/Modifiers/BackgroundParallaxHeaderModifier.swift
+++ Shared/Extensions/ViewExtensions/Modifiers/BackgroundParallaxHeaderModifier.swift
@@ -13,6 +13,9 @@ struct BackgroundParallaxHeaderModifier<Header: View>: ViewModifier {
     @Binding
     private var scrollViewOffset: CGFloat
 
+    @State
+    private var contentSize: CGSize = .zero
+
     private let height: CGFloat
     private let multiplier: CGFloat
     private let header: () -> Header
@@ -30,15 +33,18 @@ struct BackgroundParallaxHeaderModifier<Header: View>: ViewModifier {
     }
 
     func body(content: Content) -> some View {
-        content.background(alignment: .top) {
-            header()
-                .offset(y: scrollViewOffset > 0 ? -scrollViewOffset * multiplier : 0)
-                .scaleEffect(scrollViewOffset < 0 ? (height - scrollViewOffset) / height : 1, anchor: .top)
-                .mask(alignment: .top) {
-                    Color.black
-                        .frame(height: max(0, height - scrollViewOffset))
-                }
-                .ignoresSafeArea()
-        }
+        content
+            .size($contentSize)
+            .background(alignment: .top) {
+                header()
+                    .offset(y: scrollViewOffset > 0 ? -scrollViewOffset * multiplier : 0)
+                    .scaleEffect(scrollViewOffset < 0 ? (height - scrollViewOffset) / height : 1, anchor: .top)
+                    .frame(width: contentSize.width)
+                    .mask(alignment: .top) {
+                        Color.black
+                            .frame(height: max(0, height - scrollViewOffset))
+                    }
+                    .ignoresSafeArea()
+            }
     }
 }
