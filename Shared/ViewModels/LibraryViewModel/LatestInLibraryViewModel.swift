27fa79d783014a95c3c32ef2406d52dd6c0a2f5f
Ryan Patton <ryancpatton0@gmail.com>
2024-04-20 20:25:25 +0100

    get latest items instead of recently added

--- Shared/ViewModels/LibraryViewModel/LatestInLibraryViewModel.swift
+++ Shared/ViewModels/LibraryViewModel/LatestInLibraryViewModel.swift
@@ -13,20 +13,30 @@ final class LatestInLibraryViewModel: PagingLibraryViewModel<BaseItemDto>, Ident
 
     override func get(page: Int) async throws -> [BaseItemDto] {
 
-        let parameters = parameters()
-        let request = Paths.getLatestMedia(userID: userSession.user.id, parameters: parameters)
+        let parameters = parameters(for: page)
+        let request = Paths.getItemsByUserID(userID: userSession.user.id, parameters: parameters)
         let response = try await userSession.client.send(request)
 
-        return response.value
+        return response.value.items ?? []
     }
 
-    private func parameters() -> Paths.GetLatestMediaParameters {
+    private func parameters(for page: Int) -> Paths.GetItemsByUserIDParameters {
 
-        var parameters = Paths.GetLatestMediaParameters()
+        var parameters = Paths.GetItemsByUserIDParameters()
         parameters.parentID = parent?.id
-        parameters.fields = .MinimumFields
         parameters.enableUserData = true
+        parameters.fields = .MinimumFields
+        parameters.includeItemTypes = [.movie, .series]
+        parameters.isRecursive = true
         parameters.limit = pageSize
+        parameters.sortBy = [ItemSortBy.premiereDate.rawValue]
+        parameters.sortOrder = [.descending]
+        parameters.startIndex = page
+
+        // Necessary to get an actual "next page" with this endpoint.
+        // Could be a performance issue for lots of items, but there's
+        // nothing we can do about it.
+        parameters.excludeItemIDs = elements.compactMap(\.id)
 
         return parameters
     }
