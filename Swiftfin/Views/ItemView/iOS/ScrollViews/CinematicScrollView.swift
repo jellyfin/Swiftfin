9640193cac2ebf9fec667ad3b21f01c5a3d557a0
Ethan Pippin <ethanpippin2343@gmail.com>
2024-04-23 11:23:04 -0600

    wip

--- Swiftfin/Views/ItemView/iOS/ScrollViews/CinematicScrollView.swift
+++ Swiftfin/Views/ItemView/iOS/ScrollViews/CinematicScrollView.swift
@@ -44,7 +44,7 @@ extension ItemView {
                 usePrimaryImage ? .primary : .backdrop,
                 maxWidth: UIScreen.main.bounds.width
             ))
-            .aspectRatio(contentMode: .fill)
+            .aspectRatio(usePrimaryImage ? (2 / 3) : 1.77, contentMode: .fill)
             .frame(height: UIScreen.main.bounds.height * 0.6)
             .bottomEdgeGradient(bottomColor: blurHashBottomEdgeColor)
             .onAppear {
