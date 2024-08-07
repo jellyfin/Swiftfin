e3298c56e3bfe2e3d6f023d01282e0081dd34211
Ethan Pippin <ethanpippin2343@gmail.com>
2024-08-07 13:55:39 -0600

    Update String.swift

--- Shared/Extensions/String.swift
+++ Shared/Extensions/String.swift
@@ -130,18 +130,6 @@ extension String {
 
         return ceil(boundingBox.height)
     }
-
-    func width(withConstrainedHeight height: CGFloat, font: UIFont) -> CGFloat {
-        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
-        let boundingBox = self.boundingRect(
-            with: constraintRect,
-            options: .usesLineFragmentOrigin,
-            attributes: [.font: font],
-            context: nil
-        )
-
-        return ceil(boundingBox.width)
-    }
 }
 
 extension CharacterSet {
