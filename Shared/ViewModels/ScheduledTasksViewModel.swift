4e8deef6c80833c686e9f40a063ffcc6ad006553
Joe <jpkribs@outlook.com>
2024-09-19 17:46:47 -0600

    Hide scheduled tasks that are isHidden and make sure to only included enabled tasks.

--- Shared/ViewModels/ScheduledTasksViewModel.swift
+++ Shared/ViewModels/ScheduledTasksViewModel.swift
@@ -125,7 +125,7 @@ final class ScheduledTasksViewModel: ViewModel, Stateful {
     // MARK: - Fetch All Tasks
 
     private func fetchTasks() async throws {
-        let request = Paths.getTasks()
+        let request = Paths.getTasks(isHidden: false, isEnabled: true)
         let response = try await userSession.client.send(request)
 
         await MainActor.run {
