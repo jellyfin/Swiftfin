84d411fa6efbbba08261bf8ec224938756494320
Joe <jpkribs@outlook.com>
2024-11-12 14:36:57 -0700

    Update Shared/ViewModels/ItemEditorViewModel/RefreshMetadataViewModel.swift
    Co-authored-by: Ethan Pippin <ethanpippin2343@gmail.com>

--- Shared/ViewModels/ItemEditorViewModel/RefreshMetadataViewModel.swift
+++ Shared/ViewModels/ItemEditorViewModel/RefreshMetadataViewModel.swift
@@ -41,7 +41,7 @@ class RefreshMetadataViewModel: ViewModel, Stateful, Eventful {
     }
 
     @Published
-    var progress: Double = 0.0
+    private(set) var progress: Double = 0.0
 
     @Published
     private var item: BaseItemDto
