75ea0cd2584f6beeeb4ccce99e8b169afc3ced0d
Ethan Pippin <ethanpippin2343@gmail.com>
2024-12-16 18:32:02 -0700

    update

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -10,59 +10,6 @@ import Foundation
 
 class Fastfile: LaneFile {
     
-    // MARK: tag
-    
-    func tagLane(withOptions options: [String: String]?) {
-        
-        guard let options,
-                let tag = options["tag"] else {
-            puts(message: "ERROR: missing options")
-            exit(1)
-        }
-
-        guard !gitTagExists(tag: tag) else {
-            puts(message: "ERROR: tag \(tag) already exists")
-            exit(1)
-        }
-        
-        addGitTag(
-            tag: .userDefined(tag),
-            commit: .userDefined(options["commit"])
-        )
-        
-        pushGitTags(
-            force: true
-        )
-    }
-    
-    // MARK: draft release
-    
-    func draftReleaseLane(withOptions options: [String: String]?) {
-        
-        guard let options,
-                let repository = options["repository"],
-                let apiToken = options["apiToken"],
-                let tag = options["tag"],
-                let name64 = options["name64"] else {
-            puts(message: "ERROR: missing options")
-            exit(1)
-        }
-        
-        guard let name = decodeBase64(encoded: name64) else {
-            puts(message: "ERROR: name not valid base 64")
-            exit(1)
-        }
-        
-        setGithubRelease(
-            repositoryName: repository,
-            apiToken: .userDefined(apiToken),
-            tagName: tag,
-            name: .userDefined(name),
-            isDraft: true,
-            isGenerateReleaseNotes: true
-        )
-    }
-    
     // MARK: TestFlight
     
     // TODO: verify tvOS
