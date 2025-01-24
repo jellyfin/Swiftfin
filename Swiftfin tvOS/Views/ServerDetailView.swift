c8263f92f2906ca1d70a03945fa01c8dca80edd9
Joe Kribs <jpkribs@outlook.com>
2025-01-23 20:06:09 -0700

    Update ServerDetailView.swift
    Co-authored-by: Ethan Pippin <ethanpippin2343@gmail.com>

--- Swiftfin tvOS/Views/ServerDetailView.swift
+++ Swiftfin tvOS/Views/ServerDetailView.swift
@@ -76,7 +76,7 @@ struct EditServerView: View {
                         }
                     }
                     .listRowBackground(Color.clear)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    .listRowInsets(.zero)
                 }
 
                 if isEditing {
