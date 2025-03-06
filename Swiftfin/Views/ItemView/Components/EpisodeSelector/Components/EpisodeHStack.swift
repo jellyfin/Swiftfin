a274d3ce6f2cdf46d57e2bdff23679c3fbeb7767
Joe <jpkribs@outlook.com>
2025-03-05 21:37:24 -0700

    Fix Episode Scrolling on iOS as well.

--- Swiftfin/Views/ItemView/Components/EpisodeSelector/Components/EpisodeHStack.swift
+++ Swiftfin/Views/ItemView/Components/EpisodeSelector/Components/EpisodeHStack.swift
@@ -43,10 +43,14 @@ extension SeriesEpisodeSelector {
                 guard !didScrollToPlayButtonItem else { return }
                 didScrollToPlayButtonItem = true
 
-                // good enough?
                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
                     guard let playButtonItem else { return }
-                    proxy.scrollTo(element: playButtonItem, animated: false)
+
+                    if let itemIndex = viewModel.elements.firstIndex(where: { $0.id == playButtonItem.id }),
+                     itemIndex > 0
+                    {
+                        proxy.scrollTo(index: itemIndex, animated: false)
+                    }
                 }
             }
         }
