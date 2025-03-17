fb55c61ece67d263ed1f4e309c0ff61381c68afd
Joe <jpkribs@outlook.com>
2025-03-17 09:03:03 -0600

    Remove InlineEnumToggle.

--- Swiftfin tvOS/Views/SettingsView/PlaybackQualitySettingsView.swift
+++ Swiftfin tvOS/Views/SettingsView/PlaybackQualitySettingsView.swift
@@ -50,21 +50,15 @@ struct PlaybackQualitySettingsView: View {
 
                 if appMaximumBitrate == .auto {
                     Section {
-                        InlineEnumToggle(
-                            title: L10n.testSize,
-                            selection: $appMaximumBitrateTest
-                        )
+                        ListRowMenu(L10n.testSize, selection: $appMaximumBitrateTest)
                     } footer: {
                         L10n.bitrateTestDisclaimer.text
                     }
                 }
 
                 Section {
-                    InlineEnumToggle(
-                        title: L10n.compatibility,
-                        selection: $compatibilityMode
-                    )
-                    .focused($focusedItem, equals: .compatibility)
+                    ListRowMenu(L10n.compatibility, selection: $compatibilityMode)
+                        .focused($focusedItem, equals: .compatibility)
 
                     if compatibilityMode == .custom {
                         ChevronButton(L10n.profiles)
