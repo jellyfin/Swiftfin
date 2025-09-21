7847af5b6e207baad3f4e1ef4bd542bb25d31e36
Ethan Pippin <ethanpippin2343@gmail.com>
2025-09-21 16:59:30 -0400

    wip

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -101,6 +101,27 @@ class Fastfile: LaneFile {
         )
     }
     
+    func buildLane(withOptions options: [String: String]?) {
+        guard let options,
+              let scheme = options["scheme"]?.trimOption() else {
+            puts(message: "ERROR: missing or incorrect options")
+            exit(1)
+        }
+        
+        if let xcodeVersion = options["xcodeVersion"] {
+            xcodes(version: xcodeVersion)
+        }
+        
+        buildApp(
+            scheme: .userDefined(scheme),
+            exportMethod: .userDefined("development"),
+            skipArchive: .userDefined(true),
+            skipCodesigning: .userDefined(true),
+            xcargs: .userDefined("-skipMacroValidation"),
+            skipProfileDetection: true
+        )
+    }
+    
     // MARK: Utilities
     
     private func decodeBase64(encoded: String) -> String? {
