f0d28550655249e871cd02f1bc98c2382f4fc8ca
Joe Kribs <jpkribs@outlook.com>
2025-09-20 19:09:27 -0600

    Update to use Macros

--- Shared/ViewModels/AdminDashboard/ActiveSessionsViewModel.swift
+++ Shared/ViewModels/AdminDashboard/ActiveSessionsViewModel.swift
@@ -13,105 +13,57 @@ import OrderedCollections
 import SwiftUI
 
 @MainActor
-final class ActiveSessionsViewModel: ViewModel, Stateful {
+@Stateful
+final class ActiveSessionsViewModel: ViewModel {
 
-    // MARK: - Action
-
-    enum Action: Equatable {
+    @CasePathable
+    enum Action {
         case refresh
         case backgroundRefresh
-    }
 
-    // MARK: - BackgroundState
+        var transition: Transition {
+            .loop(.refreshing)
+        }
+    }
 
     enum BackgroundState: Hashable {
         case backgroundRefreshing
     }
 
-    // MARK: - State
-
-    enum State: Hashable {
-        case content
-        case error(JellyfinAPIError)
+    enum State {
         case initial
+        case error
+        case refreshing
     }
 
     @Published
     var activeWithinSeconds: Int? = 900 {
         didSet {
-            send(.refresh)
+            refresh()
         }
     }
 
     @Published
     var showSessionType: ActiveSessionFilter = .all {
         didSet {
-            send(.refresh)
+            refresh()
         }
     }
 
     @Published
-    var backgroundStates: Set<BackgroundState> = []
-    @Published
     var sessions: OrderedDictionary<String, BindingBox<SessionInfoDto?>> = [:]
-    @Published
-    var state: State = .initial
 
-    private var sessionTask: AnyCancellable?
+    @Function(\Action.Cases.refresh)
+    private func _refresh() async throws {
+        try await updateSessions()
+    }
 
-    func respond(to action: Action) -> State {
-        switch action {
-        case .backgroundRefresh:
-            sessionTask?.cancel()
+    @Function(\Action.Cases.backgroundRefresh)
+    private func _backgroundRefresh() async throws {
+        backgroundStates.insert(.backgroundRefreshing)
+        defer { backgroundStates.remove(.backgroundRefreshing) }
 
-            sessionTask = Task { [weak self] in
-                await MainActor.run {
-                    let _ = self?.backgroundStates.insert(.backgroundRefreshing)
-                }
-
-                do {
-                    try await self?.updateSessions()
-                } catch {
-                    guard let self else { return }
-                    await MainActor.run {
-                        self.state = .error(.init(error.localizedDescription))
-                    }
-                }
-
-                await MainActor.run {
-                    let _ = self?.backgroundStates.remove(.backgroundRefreshing)
-                }
-            }
-            .asAnyCancellable()
-
-            return state
-        case .refresh:
-            sessionTask?.cancel()
-
-            sessionTask = Task { [weak self] in
-                await MainActor.run {
-                    self?.state = .initial
-                }
-
-                do {
-                    try await self?.updateSessions()
-
-                    guard let self else { return }
-
-                    await MainActor.run {
-                        self.state = .content
-                    }
-                } catch {
-                    guard let self else { return }
-                    await MainActor.run {
-                        self.state = .error(.init(error.localizedDescription))
-                    }
-                }
-            }
-            .asAnyCancellable()
-
-            return .initial
-        }
+        try await updateSessions()
     }
 
     // MARK: - updateSessions
@@ -153,45 +105,43 @@ final class ActiveSessionsViewModel: ViewModel, Stateful {
                 )
             }
 
-        await MainActor.run {
-            for id in removedSessionIDs {
-                let t = sessions[id]
-                sessions[id] = nil
-                t?.value = nil
+        for id in removedSessionIDs {
+            let t = sessions[id]
+            sessions[id] = nil
+            t?.value = nil
+        }
+
+        for id in existingIDs {
+            sessions[id]?.value = filteredSessions.first(where: { $0.id == id })
+        }
+
+        for session in newSessions {
+            guard let id = session.value?.id else { continue }
+
+            sessions[id] = session
+        }
+
+        sessions.sort { x, y in
+            let xs = x.value.value
+            let ys = y.value.value
+
+            let isPlaying0 = xs?.nowPlayingItem != nil
+            let isPlaying1 = ys?.nowPlayingItem != nil
+
+            if isPlaying0 && !isPlaying1 {
+                return true
+            } else if !isPlaying0 && isPlaying1 {
+                return false
             }
 
-            for id in existingIDs {
-                sessions[id]?.value = filteredSessions.first(where: { $0.id == id })
+            if xs?.userName != ys?.userName {
+                return (xs?.userName ?? "") < (ys?.userName ?? "")
             }
 
-            for session in newSessions {
-                guard let id = session.value?.id else { continue }
-
-                sessions[id] = session
-            }
-
-            sessions.sort { x, y in
-                let xs = x.value.value
-                let ys = y.value.value
-
-                let isPlaying0 = xs?.nowPlayingItem != nil
-                let isPlaying1 = ys?.nowPlayingItem != nil
-
-                if isPlaying0 && !isPlaying1 {
-                    return true
-                } else if !isPlaying0 && isPlaying1 {
-                    return false
-                }
-
-                if xs?.userName != ys?.userName {
-                    return (xs?.userName ?? "") < (ys?.userName ?? "")
-                }
-
-                if isPlaying0 && isPlaying1 {
-                    return (xs?.nowPlayingItem?.name ?? "") < (ys?.nowPlayingItem?.name ?? "")
-                } else {
-                    return (xs?.lastActivityDate ?? Date.now) > (ys?.lastActivityDate ?? Date.now)
-                }
+            if isPlaying0 && isPlaying1 {
+                return (xs?.nowPlayingItem?.name ?? "") < (ys?.nowPlayingItem?.name ?? "")
+            } else {
+                return (xs?.lastActivityDate ?? Date.now) > (ys?.lastActivityDate ?? Date.now)
             }
         }
     }
