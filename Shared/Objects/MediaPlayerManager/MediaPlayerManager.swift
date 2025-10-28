f29189620280fb895af98851615e33fe0e18fe2f
Joe Kribs <jpkribs@icloud.com>
2025-10-28 13:52:19 -0600

    Update stop and playNewItem calls to use await

--- Shared/Objects/MediaPlayerManager/MediaPlayerManager.swift
+++ Shared/Objects/MediaPlayerManager/MediaPlayerManager.swift
@@ -221,7 +221,7 @@ final class MediaPlayerManager: ViewModel {
         // is verifiable by given seconds being near item runtime.
         // VLC proxy will send ended early.
         guard let runtime = item.runtime else {
-            try await self.stop()
+            await self.stop()
             return
         }
         let isNearEnd = (runtime - seconds) <= .seconds(1)
@@ -232,7 +232,7 @@ final class MediaPlayerManager: ViewModel {
         }
 
         if let nextItem = queue?.nextItem, Defaults[.VideoPlayer.autoPlayEnabled] {
-            try await self.playNewItem(provider: nextItem)
+            await self.playNewItem(provider: nextItem)
         }
     }
 
@@ -295,7 +295,7 @@ final class MediaPlayerManager: ViewModel {
     @Function(\Action.Cases.start)
     private func _start() async throws {
         guard let initialMediaPlayerItemProvider else {
-            try await self.stop()
+            await self.stop()
             return
         }
         self.initialMediaPlayerItemProvider = nil
