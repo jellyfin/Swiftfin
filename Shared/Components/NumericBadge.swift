0ec123f26edc94ea1992b555078ece8cfab407f8
AlexMoralesDev <alex.morales8@upr.edu>
2025-11-22 21:50:38 -0400

    Made tvOS badge bigger

--- Shared/Components/NumericBadge.swift
+++ Shared/Components/NumericBadge.swift
@@ -15,19 +15,32 @@ struct NumericBadge: View {
     private var size: CGFloat {
         let font = UIFont.systemFont(ofSize: 11, weight: .bold)
         let textSize = content.size(withAttributes: [.font: font])
-        return max(textSize.width + 8, textSize.height + 4)
+        let baseSize = max(textSize.width + 8, textSize.height + 4)
+        #if os(tvOS)
+        return baseSize * 2
+        #else
+        return baseSize
+        #endif
+    }
+
+    private var fontSize: CGFloat {
+        #if os(tvOS)
+        return 22
+        #else
+        return 11
+        #endif
     }
 
     var body: some View {
         Text(content)
-            .font(.system(size: 11, weight: .bold))
+            .font(.system(size: fontSize, weight: .bold))
             .foregroundStyle(.white)
             .frame(width: size, height: size)
             .background(.black.opacity(0.8))
             .clipShape(.circle)
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-            .padding(.top, 2)
-            .padding(.leading, 2)
+            .padding(.top, 4)
+            .padding(.leading, 4)
     }
 }
 
