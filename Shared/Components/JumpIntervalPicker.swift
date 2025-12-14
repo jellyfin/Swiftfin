a1973fcde8e821a506700ffda2d1a68ed409b601
Joe <jpkribs@outlook.com>
2025-12-14 16:18:35 -0700

    Revert

--- Shared/Components/JumpIntervalPicker.swift
+++ Shared/Components/JumpIntervalPicker.swift
@@ -10,6 +10,11 @@ import SwiftUI
 
 struct JumpIntervalPicker: View {
 
+    @State
+    private var customSeconds: Int = 0
+    @State
+    private var isPresentingCustomInterval = false
+
     let title: String
     let selection: Binding<MediaJumpInterval>
 
@@ -18,60 +23,119 @@ struct JumpIntervalPicker: View {
         self.selection = selection
     }
 
-    var body: some View {
-        contentView
+    private var mappedSelection: Binding<Duration> {
+        selection.map(
+            getter: {
+                if case .custom = $0 { .zero } else { $0.rawValue }
+            },
+            setter: {
+                MediaJumpInterval(rawValue: $0)
+            }
+        )
     }
 
     @ViewBuilder
-    private var contentView: some View {
+    private var pickerContent: some View {
+        ForEach(MediaJumpInterval.allCases, id: \.hashValue) { interval in
+            Text(interval.rawValue, format: .minuteSecondsNarrow)
+                .tag(interval.rawValue)
+        }
+
+        Divider()
+
+        Text(L10n.custom)
+            .tag(Duration.zero)
+    }
+
+    @ViewBuilder
+    private var picker: some View {
+        if #available(iOS 18.0, tvOS 18.0, *) {
+            Picker(title, selection: mappedSelection) {
+                pickerContent
+            } currentValueLabel: {
+                Text(selection.wrappedValue.rawValue, format: .minuteSecondsNarrow)
+            }
+        } else {
+            Picker(title, selection: mappedSelection) {
+                pickerContent
+            }
+        }
+    }
+
+    /* TODO: Remove the 3 var above this in favor of the picker below for iOS 18+
+     @ViewBuilder
+     private var picker: some View {
+         Picker(
+             title,
+             selection: selection.map(
+                 getter: {
+                     if case .custom = $0 {
+                         return .zero
+                     } else {
+                         return $0.rawValue
+                     }
+                 },
+                 setter: {
+                     MediaJumpInterval(rawValue: $0)
+                 }
+             )
+         ) {
+             ForEach(MediaJumpInterval.allCases, id: \.hashValue) { interval in
+                 Text(interval.rawValue, format: .minuteSecondsNarrow)
+                     .tag(interval.rawValue)
+             }
+
+             Divider()
+
+             Text(L10n.custom)
+                 .tag(Duration.zero)
+         } currentValueLabel: {
+             Text(selection.wrappedValue.rawValue, format: .minuteSecondsNarrow)
+         }
+     }
+     */
+
+    @ViewBuilder
+    private var content: some View {
         #if os(tvOS)
-        ListRowMenu(title, subtitle: Text(selection.wrappedValue.displayTitle)) {
-            pickerView
+        ListRowMenu(title, subtitle: Text(selection.wrappedValue.rawValue, format: .minuteSecondsNarrow)) {
+            picker
         }
         #else
-        pickerView
+        picker
         #endif
     }
 
-    @ViewBuilder
-    private var pickerView: some View {
-        Picker(
-            title,
-            selection: selection,
-            currentValue: selection.wrappedValue.displayTitle,
-            isCustom: { if case .custom = $0 { true } else { false } },
-            customTag: .custom(interval: .zero),
-            customDefault: { .custom(interval: $0.rawValue) },
-            customDescription: L10n.customJumpIntervalDescription
-        ) {
-            ForEach(MediaJumpInterval.allCases, id: \.hashValue) { interval in
-                Text(interval.displayTitle)
-                    .tag(interval)
+    var body: some View {
+        content
+            .backport
+            .onChange(of: selection.wrappedValue) { oldValue, newValue in
+                if case let .custom(interval) = newValue {
+                    if interval == .zero {
+                        customSeconds = Int(oldValue.rawValue.seconds)
+                        isPresentingCustomInterval = true
+                    } else {
+                        if let matchingStatic = MediaJumpInterval.allCases.first(where: { $0.rawValue == interval }) {
+                            selection.wrappedValue = matchingStatic
+                        }
+                    }
+                }
             }
-        } customInput: { $value in
-            TextField(L10n.duration, value: Binding(
-                get: {
-                    if case let .custom(interval) = value {
-                        Int(interval.components.seconds)
-                    } else {
-                        Int(value.rawValue.components.seconds)
+            .alert(L10n.jump, isPresented: $isPresentingCustomInterval) {
+                TextField(L10n.duration, value: $customSeconds, format: .number)
+                    .keyboardType(.numberPad)
+                    .backport
+                    .onChange(of: customSeconds) { _, newValue in
+                        if newValue < 1 {
+                            customSeconds = 1
+                        }
                     }
-                },
-                set: { seconds in
-                    if let matching = MediaJumpInterval.allCases.first(where: { $0.rawValue == .seconds(seconds) }) {
-                        value = matching
-                    } else {
-                        value = .custom(interval: .seconds(seconds))
-                    }
+
+                Button(L10n.save) {
+                    selection.wrappedValue = .custom(interval: .seconds(customSeconds))
                 }
-            ), format: .number)
-                .keyboardType(.numberPad)
-                .backport
-                .onChange(of: value) { _, newValue in
-                    if newValue.rawValue.components.seconds < 1 {
-                        value = .custom(interval: .seconds(1))
-                    }
-                }
-        }
+            } message: {
+                Text(L10n.customJumpIntervalDescription)
+            }
     }
 }
