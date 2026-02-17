ffded98c022804b88886840cd7d39e911fcc797c
Henry Lightfoot <henryauryn@icloud.com>
2026-02-17 14:10:27 +0000

    making modal only focusable when scrollable

--- Shared/Extensions/ViewExtensions/Modifiers/ScrollIfLargerThanContainerModifier.swift
+++ Shared/Extensions/ViewExtensions/Modifiers/ScrollIfLargerThanContainerModifier.swift
@@ -1,41 +1,41 @@
 //
-// Swiftfin is subject to the terms of the Mozilla Public
-// License, v2.0. If a copy of the MPL was not distributed with this
-// file, you can obtain one at https://mozilla.org/MPL/2.0/.
-//
-// Copyright (c) 2026 Jellyfin & Jellyfin Contributors
-//
+  // Swiftfin is subject to the terms of the Mozilla Public
+  // License, v2.0. If a copy of the MPL was not distributed with this
+  // file, you can obtain one at https://mozilla.org/MPL/2.0/.
+  //
+  // Copyright (c) 2026 Jellyfin & Jellyfin Contributors
+  //
+                  
+  import SwiftUI
 
-import SwiftUI
+  struct ScrollIfLargerThanContainerModifier: ViewModifier {
+   
+      @State
+      private var contentSize: CGSize = .zero
 
-struct ScrollIfLargerThanContainerModifier: ViewModifier {
+      let axes: Axis.Set
+      let padding: CGFloat
 
-    @State
-    private var contentSize: CGSize = .zero
-
-    let axes: Axis.Set
-    let padding: CGFloat
-
-    func body(content: Content) -> some View {
-        AlternateLayoutView {
-            Color.clear
-        } content: { layoutSize in
-
-            let isHorizontallyLarger: Bool = (contentSize.width + padding >= layoutSize.width) && axes.contains(.horizontal)
-            let isVerticallyLarger: Bool = (contentSize.height + padding >= layoutSize.height) && axes.contains(.vertical)
-
-            ScrollView(axes) {
-                content
-                    .trackingSize($contentSize)
-            }
-            .frame(
-                maxWidth: axes.contains(.horizontal) ? (isHorizontallyLarger ? .infinity : contentSize.width) : nil,
-                maxHeight: axes.contains(.vertical) ? (isVerticallyLarger ? .infinity : contentSize.height) : nil
-            )
-            .backport // iOS 17
-            .scrollClipDisabled()
-            .scrollDisabled((axes.contains(.horizontal) && !isHorizontallyLarger) || (axes.contains(.vertical) && !isVerticallyLarger))
-            .scrollIndicators(.never)
-        }
-    }
-}
+      func body(content: Content) -> some View {
+          AlternateLayoutView {
+              Color.clear
+          } content: { layoutSize in
+                                                                                                                                            
+              let isHorizontallyLarger: Bool = (contentSize.width + padding >= layoutSize.width) && axes.contains(.horizontal)
+              let isVerticallyLarger: Bool = (contentSize.height + padding >= layoutSize.height) && axes.contains(.vertical)
+                                                                                                                                            
+              ScrollView(axes) {
+                  content
+                      .trackingSize($contentSize)
+              }
+              .frame(
+                  maxWidth: axes.contains(.horizontal) ? (isHorizontallyLarger ? .infinity : contentSize.width) : nil,
+                  maxHeight: axes.contains(.vertical) ? (isVerticallyLarger ? .infinity : contentSize.height) : nil
+              )
+              .backport // iOS 17
+              .scrollClipDisabled()
+              .scrollDisabled((axes.contains(.horizontal) && !isHorizontallyLarger) || (axes.contains(.vertical) && !isVerticallyLarger))
+              .scrollIndicators(.never)
+          }
+      }
+  }   
