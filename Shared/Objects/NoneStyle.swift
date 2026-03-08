22ec53923a9e4dda9f5982a36e9dcfa6271dec7a
Joe Kribs <jpkribs@icloud.com>
2026-03-07 20:20:08 -0700

    Unify `CustomizationSettingsView` (#1857)
    Co-authored-by: Ethan Pippin <ethanpippin2343@gmail.com>

---
+++ Shared/Objects/NoneStyle.swift
@@ -0,0 +1,27 @@
+//
+// Swiftfin is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2026 Jellyfin & Jellyfin Contributors
+//
+
+enum NoneStyle: Displayable {
+
+    case text
+    case dash(Int)
+    case custom(String)
+
+    var displayTitle: String {
+        switch self {
+        case .text:
+            return L10n.none
+        case let .dash(length):
+            assert(length >= 1, "Dash must have length of at least 1.")
+            return String(repeating: "-", count: length)
+        case let .custom(text):
+            assert(text.isNotEmpty, "Custom text must have length of at least 1.")
+            return text
+        }
+    }
+}
