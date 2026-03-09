43f11054cd9b91b7513844052ce68b32289e9c52
Joe <jpkribs@outlook.com>
2026-03-09 14:17:59 -0600

    Fix tvOS picker. Just use build flags since its not worth a new init for just this.

--- Shared/Views/AppSettingsView.swift
+++ Shared/Views/AppSettingsView.swift
@@ -13,12 +13,6 @@ import SwiftUI
 
 struct AppSettingsView: View {
 
-    #if os(tvOS)
-    typealias PlatformPicker = ListRowMenu
-    #else
-    typealias PlatformPicker = Picker
-    #endif
-
     @Default(.selectUserUseSplashscreen)
     private var selectUserUseSplashscreen
     @Default(.selectUserAllServersSplashscreen)
@@ -41,6 +35,26 @@ struct AppSettingsView: View {
 
     @StateObject
     private var viewModel = SettingsViewModel()
+
+    #if os(tvOS)
+    private var selectedServer: ServerState? {
+        viewModel.servers.first { server in
+            selectUserAllServersSplashscreen == .server(id: server.id)
+        }
+    }
+    #endif
+
+    private var serverPicker: some View {
+        Picker(L10n.servers, selection: $selectUserAllServersSplashscreen) {
+            Label(L10n.random, systemImage: "dice.fill")
+                .tag(SelectUserServerSelection.all)
+
+            ForEach(viewModel.servers) { server in
+                Text(server.name)
+                    .tag(SelectUserServerSelection.server(id: server.id))
+            }
+        }
+    }
 
     var body: some View {
         Form(image: .jellyfinBlobBlue) {
@@ -72,15 +86,22 @@ struct AppSettingsView: View {
                 Toggle(L10n.useSplashscreen, isOn: $selectUserUseSplashscreen)
 
                 if selectUserUseSplashscreen {
-                    PlatformPicker(L10n.servers, selection: $selectUserAllServersSplashscreen) {
-                        Label(L10n.random, systemImage: "dice.fill")
-                            .tag(SelectUserServerSelection.all)
 
-                        ForEach(viewModel.servers) { server in
-                            Text(server.name)
-                                .tag(SelectUserServerSelection.server(id: server.id))
+                    #if os(tvOS)
+                    ListRowMenu(L10n.servers) {
+                        if selectUserAllServersSplashscreen == .all {
+                            Label(L10n.random, systemImage: "dice.fill")
+                        } else if let selectedServer {
+                            Text(selectedServer.name)
+                        } else {
+                            Text(L10n.none)
                         }
+                    } content: {
+                        serverPicker
                     }
+                    #else
+                    serverPicker
+                    #endif
                 }
             } header: {
                 Text(L10n.splashscreen)
