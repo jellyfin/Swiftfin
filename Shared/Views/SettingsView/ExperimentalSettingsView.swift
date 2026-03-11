1a0e97366ded1c736da95c14b6ac7a0e7a847123
Joe Kribs <jpkribs@icloud.com>
2026-03-10 23:16:05 -0600

    Reduce to just the view

--- Shared/Views/SettingsView/ExperimentalSettingsView.swift
+++ Shared/Views/SettingsView/ExperimentalSettingsView.swift
@@ -12,62 +12,11 @@ import SwiftUI
 /// `Note`: Used for experimental settings that may be removed or implemented officially. Keep for future settings.
 struct ExperimentalSettingsView: View {
 
-    // MARK: - iOS Settings
-
-    #if os(iOS)
-
-    // Enable if `ExperimentalSettingsView` is needed for iOS Settings
-    private static let hasPlatformSettings = false
+    private static let isEnabled = false
 
     @ViewBuilder
-    private var platformSettings: some View {
-        // iOS Specific Experimental Settings Below
-    }
-
-    // MARK: - tvOS Settings
-
-    #elseif os(tvOS)
-
-    // Enable if `ExperimentalSettingsView` is needed for tvOS Settings
-    private static let hasPlatformSettings = false
-
-    @ViewBuilder
-    private var platformSettings: some View {
-        // tvOS Specific Experimental Settings Below
-    }
-    #endif
-
-    // MARK: - Shared Settings
-
-    // Enable if `ExperimentalSettingsView` is needed for Shared Settings
-    private static let hasSharedSettings = false
-
-    @ViewBuilder
-    private var sharedSettings: some View {
-        // Non-OS Specific Experimental Settings Below
-    }
-
-    // MARK: - Boilerplate (Do Not Change)
-
-    static var isEnabled: Bool {
-        hasPlatformSettings || hasSharedSettings
-    }
-
     var body: some View {
-        Form(systemImage: "flask") {
-
-            if Self.hasPlatformSettings {
-                Section("Platform") {
-                    platformSettings
-                }
-            }
-
-            if Self.hasSharedSettings {
-                Section("Swiftfin") {
-                    sharedSettings
-                }
-            }
-        }
-        .navigationTitle(L10n.experimental)
+        Form(systemImage: "flask") {}
+            .navigationTitle(L10n.experimental)
     }
 }
