846a2579f5d1ad32f1b8c2138ccccb9491711d48
Joe <jpkribs@outlook.com>
2026-03-13 14:33:11 -0600

    Remove duplicates

--- Swiftfin/Views/ItemEditorView/ItemSubtitles/ItemSubtitleSearchView/ItemSubtitleSearchView.swift
+++ Swiftfin/Views/ItemEditorView/ItemSubtitles/ItemSubtitleSearchView/ItemSubtitleSearchView.swift
@@ -137,11 +137,9 @@ struct ItemSubtitleSearchView: View {
     private var resultsSection: some View {
         Section(L10n.search) {
             if viewModel.searchResults.isEmpty {
-                if viewModel.searchResults.isEmpty {
-                    Text(L10n.none)
-                        .foregroundStyle(.secondary)
-                        .frame(maxWidth: .infinity, alignment: .center)
-                }
+                Text(L10n.none)
+                    .foregroundStyle(.secondary)
+                    .frame(maxWidth: .infinity, alignment: .center)
             }
             ForEach(viewModel.searchResults, id: \.id) { subtitle in
                 let isSelected = subtitle.id.map { selectedSubtitles.contains($0) } ?? false
