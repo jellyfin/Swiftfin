b3f9b3a26d1b083c92fb50e5d2f97d3814977dc0
Joe Kribs <jpkribs@icloud.com>
2026-03-17 16:27:36 -0600

    Cleanup Deletion Logic (#1940)
    Co-authored-by: Ethan Pippin <ethanpippin2343@gmail.com>

--- Shared/Views/ItemView/Components/ItemEditorMenu.swift
+++ Shared/Views/ItemView/Components/ItemEditorMenu.swift
@@ -6,6 +6,7 @@
 // Copyright (c) 2026 Jellyfin & Jellyfin Contributors
 //
 
+import Defaults
 import JellyfinAPI
 import SwiftUI
 
@@ -63,5 +64,27 @@ struct ItemEditorMenu: View {
                 #endif
             }
         }
+
+        // TODO: Enable & Complete Deletion for tvOS after
+        /* #if os(tvOS)
+         if StoredValues[.User.enableItemDeletion] && viewModel.item.canDelete == true {
+             Button(L10n.delete, systemImage: "trash", role: .destructive) {
+                 isPresentingDeleteConfirmation.wrappedValue = true
+             }
+             .confirmationDialog(
+                 L10n.deleteItemConfirmationMessage,
+                 isPresented: $isPresentingDeleteConfirmation,
+                 titleVisibility: .visible
+             ) {
+                 Button(
+                     L10n.confirm,
+                     role: .destructive,
+                     action: viewModel.delete
+                 )
+
+                 Button(L10n.cancel, role: .cancel) {}
+             }
+         }
+         #endif */
     }
 }
