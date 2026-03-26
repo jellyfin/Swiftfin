a82961cec432faf9f838c44cec2cbd1b29e6ee95
Joe <jpkribs@outlook.com>
2026-03-26 10:11:01 -0600

    Revert `scrollIfLargerThanContainer` back to Main

--- Shared/Extensions/ViewExtensions/Modifiers/ScrollIfLargerThanContainerModifier.swift
+++ Shared/Extensions/ViewExtensions/Modifiers/ScrollIfLargerThanContainerModifier.swift
@@ -1,63 +1,41 @@
 //
-  // Swiftfin is subject to the terms of the Mozilla Public
-  // License, v2.0. If a copy of the MPL was not distributed with this
-  // file, you can obtain one at https://mozilla.org/MPL/2.0/.
-  //
-  // Copyright (c) 2026 Jellyfin & Jellyfin Contributors
-  //
-                  
-  import SwiftUI
+// Swiftfin is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2026 Jellyfin & Jellyfin Contributors
+//
 
-  struct ScrollIfLargerThanContainerModifier: ViewModifier {
-   
-      @State
-      private var contentSize: CGSize = .zero
+import SwiftUI
 
-      let axes: Axis.Set
-      let padding: CGFloat
-
-<<<<<<< HEAD
-      func body(content: Content) -> some View {
-          AlternateLayoutView {
-              Color.clear
-          } content: { layoutSize in
-                                                                                                                                            
-              let isHorizontallyLarger: Bool = (contentSize.width + padding >= layoutSize.width) && axes.contains(.horizontal)
-              let isVerticallyLarger: Bool = (contentSize.height + padding >= layoutSize.height) && axes.contains(.vertical)
-                                                                                                                                            
-              ScrollView(axes) {
-                  content
-                      .trackingSize($contentSize)
-              }
-              .frame(
-                  maxWidth: axes.contains(.horizontal) ? (isHorizontallyLarger ? .infinity : contentSize.width) : nil,
-                  maxHeight: axes.contains(.vertical) ? (isVerticallyLarger ? .infinity : contentSize.height) : nil
-              )
-              .backport // iOS 17
-              .scrollClipDisabled()
-              .scrollDisabled((axes.contains(.horizontal) && !isHorizontallyLarger) || (axes.contains(.vertical) && !isVerticallyLarger))
-              .scrollIndicators(.never)
-          }
-      }
-  }   
-=======
 struct ScrollIfLargerThanContainerModifier: ViewModifier {
 
+    @State
+    private var contentSize: CGSize = .zero
+
+    let axes: Axis.Set
     let padding: CGFloat
 
     func body(content: Content) -> some View {
-        ViewThatFits(in: .vertical) {
-            // if content is small
-            content
+        AlternateLayoutView {
+            Color.clear
+        } content: { layoutSize in
 
-            // if content too tall
-            ScrollView {
+            let isHorizontallyLarger: Bool = (contentSize.width + padding >= layoutSize.width) && axes.contains(.horizontal)
+            let isVerticallyLarger: Bool = (contentSize.height + padding >= layoutSize.height) && axes.contains(.vertical)
+
+            ScrollView(axes) {
                 content
+                    .trackingSize($contentSize)
             }
+            .frame(
+                maxWidth: axes.contains(.horizontal) ? (isHorizontallyLarger ? .infinity : contentSize.width) : nil,
+                maxHeight: axes.contains(.vertical) ? (isVerticallyLarger ? .infinity : contentSize.height) : nil
+            )
             .backport // iOS 17
             .scrollClipDisabled()
+            .scrollDisabled((axes.contains(.horizontal) && !isHorizontallyLarger) || (axes.contains(.vertical) && !isVerticallyLarger))
             .scrollIndicators(.never)
         }
     }
 }
->>>>>>> 5d546e03 (Add auto-scrolling Ticker to LearnMoreModal on tvOS)
