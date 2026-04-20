847db231b3ac88013e2b00d0cf9fd104d18c7782
Joe <jpkribs@outlook.com>
2026-04-19 20:33:07 -0600

    Safely wrap `isLiquidGlassEnabled`

--- Swiftfin tvOS/App/SwiftfinApp.swift
+++ Swiftfin tvOS/App/SwiftfinApp.swift
@@ -20,7 +20,9 @@ struct SwiftfinApp: App {
 
     init() {
 
+        #if DEBUG
         SwizzleDefaults.set(Defaults[.isLiquidGlassEnabled], for: "com.apple.SwiftUI.IgnoreSolariumOptOut")
+        #endif
 
         // Logging
         LoggingSystem.bootstrap { label in
