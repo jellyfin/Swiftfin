f51524a930e93087dc252c4696590d2c929b51ad
Joe <jpkribs@outlook.com>
2026-04-27 16:32:52 -0600

    Utilize `ScaledMetric` for better scaling

--- Swiftfin/Components/LogEntryButton.swift
+++ Swiftfin/Components/LogEntryButton.swift
@@ -11,6 +11,11 @@ import SwiftUI
 
 struct LogEntryButton<LeadingContent: View>: View {
 
+    @ScaledMetric
+    private var avatarSize: CGFloat = 60
+    @ScaledMetric
+    private var spacing: CGFloat = 4
+
     private let title: String
     private let logLevel: LogLevel
     private let contents: String
@@ -19,54 +24,47 @@ struct LogEntryButton<LeadingContent: View>: View {
     private let action: () -> Void
 
     var body: some View {
-        ChevronButton(action: action) {
-            LabeledContent {
-                AlternateLayoutView(alignment: .leading) {
-                    Color.clear
-                        .frame(minHeight: 60)
-                } content: {
-                    rowContent
-                        .frame(minHeight: 60)
+        VStack(spacing: 0) {
+            ChevronButton(action: action) {
+                LabeledContent {
+                    AlternateLayoutView(alignment: .leading) {
+                        rowContent
+                    } content: {
+                        rowContent
+                    }
+                    .layoutPriority(1)
+                } label: {
+                    leadingContent()
+                        .frame(width: avatarSize, height: avatarSize, alignment: .center)
                 }
-            } label: {
-                leadingContent()
-                    .frame(width: 60, height: 60, alignment: .center)
             }
         }
     }
 
     @ViewBuilder
     private var rowContent: some View {
-        HStack {
-            VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: spacing) {
 
-                // Header
-                HStack(spacing: 4) {
-                    Image(systemName: logLevel.systemImage)
-                        .foregroundStyle(logLevel.color)
+            HStack(spacing: spacing) {
+                Image(systemName: logLevel.systemImage)
+                    .foregroundStyle(logLevel.color)
 
-                    Text(title)
-                        .foregroundStyle(.primary)
-                        .truncationMode(.middle)
-                }
-                .font(.headline)
-
-                // Contents
-                Text(contents)
-                    .font(.subheadline)
-
-                // Timestamp
-                Text(timestamp?.formatted(date: .abbreviated, time: .standard) ?? .emptyRuntime)
-                    .font(.caption)
-
-                // List row separator for usage in a CollectionVGrid.
-                // Ensure that it goes all the way to the trailing edge of the screen.
-                Divider()
-                    .padding(.trailing, -(EdgeInsets.edgeInsets.trailing + EdgeInsets.edgePadding))
-                    .frame(alignment: .bottom)
+                Text(title)
+                    .foregroundStyle(.primary)
+                    .truncationMode(.middle)
             }
-            Spacer(minLength: 0)
+            .font(.headline)
+
+            Text(contents)
+                .font(.subheadline)
+
+            Text(timestamp?.formatted(date: .abbreviated, time: .standard) ?? .emptyRuntime)
+                .font(.caption)
+
+            Divider()
+                .padding(.trailing, -(EdgeInsets.edgePadding + EdgeInsets.edgeInsets.trailing))
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
         .lineLimit(1)
         .foregroundStyle(.secondary)
     }
