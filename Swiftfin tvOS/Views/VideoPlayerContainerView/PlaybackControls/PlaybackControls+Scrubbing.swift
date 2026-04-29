7a8fd8d1ea59e854c7df79e9c8743f1bbdfdf25f
Joe <jpkribs@outlook.com>
2026-04-28 21:58:52 -0600

    Controlled Jumps

--- Swiftfin tvOS/Views/VideoPlayerContainerView/PlaybackControls/PlaybackControls+Scrubbing.swift
+++ Swiftfin tvOS/Views/VideoPlayerContainerView/PlaybackControls/PlaybackControls+Scrubbing.swift
@@ -102,24 +102,12 @@ extension VideoPlayer.PlaybackControls {
 
         let work = DispatchWorkItem { [weak manager, weak containerState] in
             let totalDuration = interval * jumpCount
-            let currentSeconds = manager?.seconds ?? .zero
 
             switch direction {
             case .forward:
-                if let runtime = manager?.item.runtime {
-                    let remaining = max(.zero, runtime - currentSeconds)
-                    let clamped = min(totalDuration, remaining)
-                    if clamped > .zero {
-                        manager?.proxy?.jumpForward(clamped)
-                    }
-                } else {
-                    manager?.proxy?.jumpForward(totalDuration)
-                }
+                manager?.jumpForward(totalDuration)
             case .backward:
-                let clamped = min(totalDuration, currentSeconds)
-                if clamped > .zero {
-                    manager?.proxy?.jumpBackward(clamped)
-                }
+                manager?.jumpBackward(totalDuration)
             }
             containerState?.jumpProgressObserver.reset()
         }
