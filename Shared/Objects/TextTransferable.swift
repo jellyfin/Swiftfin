a657e17f907df48ebb6bee4323bb6e3278a10a08
Joe Kribs <jpkribs@icloud.com>
2026-08-07 21:42:38 -0600

    Sharable `MediaSource` / `MediaStream` (#2184)

---
+++ Shared/Objects/TextTransferable.swift
@@ -0,0 +1,55 @@
+//
+// Swiftfin is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2026 Jellyfin & Jellyfin Contributors
+//
+
+import CoreTransferable
+import Foundation
+import SwiftUI
+import UniformTypeIdentifiers
+
+public protocol TextTransferable: Transferable {
+
+    var transferTitle: String { get }
+    var transferBody: String { get }
+}
+
+public extension TextTransferable {
+
+    static var transferRepresentation: some TransferRepresentation {
+        FileRepresentation(exportedContentType: .plainText) { (item: Self) in
+            let url = URL.temporaryDirectory.appending(path: item.transferTitle.appending(".txt"))
+            try item.transferBody.write(to: url, atomically: true, encoding: .utf8)
+
+            return SentTransferredFile(url)
+        }
+    }
+
+    // TODO: Switch to this for iOS 17+
+//      static var transferRepresentation: some TransferRepresentation {
+//          DataRepresentation(exportedContentType: .plainText) { (item: Self) in
+//              Data(item.transferBody.utf8)
+//          }
+//          .suggestedFileName { $0.transferTitle.appending(".txt") }
+//      }
+
+    #if os(iOS)
+    var shareLink: some View {
+        ShareLink(
+            item: self,
+            preview: SharePreview(transferTitle)
+        )
+    }
+
+    func shareLink(@ViewBuilder label: () -> some View) -> some View {
+        ShareLink(
+            item: self,
+            preview: SharePreview(transferTitle),
+            label: label
+        )
+    }
+    #endif
+}
